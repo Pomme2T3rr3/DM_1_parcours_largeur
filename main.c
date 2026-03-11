fdcf7e5bee437534807cf599fab37a00d7e6aa06
Pomme2T3rr3 <damian.deneuville@gmail.com>
2026-03-11 12:03:04 +0100

    Update main.c

--- main.c
+++ main.c
@@ -45,3 +45,81 @@ Cellule* extrait_tete(Liste * l) {
     }
     return tmp;
 }
+
+
+void affiche_liste_renversee(Liste lst) {
+    printf("%d, ", lst->noeud->valeur);
+
+    if (!lst->suivant) {
+        return;
+    }
+
+    affiche_liste_renversee(lst->suivant);
+}
+
+
+File initialisation(void) {
+    File f = malloc(sizeof(Queue));
+    if (!f) return NULL;
+
+    f->debut = NULL;
+    f->fin = NULL;
+    f->taille = 0;
+
+    return f;
+}
+
+
+int est_vide(File f) {
+    if (!f->debut)
+        return 1;
+    else return 0;
+}
+
+
+int enfiler(File f, Noeud* n) {
+    Cellule* new = alloue_cellule(n);
+    if (!new) return 0;
+
+    if (!f->fin) {
+        f->debut = new;
+        f->fin = new;
+    } else {
+        f->fin->suivant = new;
+        f->fin = new;
+    }
+
+    f->taille++;
+
+    return 1;
+}
+
+
+Noeud* alloue_noeud(int val, Arbre fg, Arbre fd) {
+    Noeud* new = malloc(sizeof(Noeud));
+
+    if (!(new)) return NULL;
+
+    new->valeur = val;
+    new->fd = fd;
+    new->fg = fg;
+
+    return new;
+}
+
+
+int main (void) {
+    Arbre racine = alloue_noeud(1, NULL, NULL);
+    Arbre fg = alloue_noeud(2, NULL, NULL);
+    Arbre fd = alloue_noeud(3, NULL, NULL);
+    racine->fg = fg;
+    racine->fd = fd;
+
+    // test enfiler
+    File f = initialisation();
+    enfiler(f, racine);
+    enfiler(f, fg);
+    enfiler(f, fd);
+    printf("%d\n", f->taille);
+    free(f);
+}
